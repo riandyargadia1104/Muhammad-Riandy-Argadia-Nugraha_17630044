1a9e123d46b4c7c14342d1a45913e5df5a248a48
riandyargadia1104 <riandysmesa@gmail.com>
2021-03-11 16:04:57 +0800

    commit pertama praktikum Muhammad Riandy Argadia Nugraha - 17630044

---
+++ profile.php
@@ -0,0 +1,52 @@
+<?php include "header.php" ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/Boy.jpg" alt="Muhammad Riandy Argadia Nugraha" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM   :</strong></td>
+                        <td align="left" width="50%">17630044  </td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Riandy Argadia Nugraha</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 11 April 1998</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">085350569804</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">riandysmesa@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://web.facebook.com/arga.nugraha.39/" target="_blank" style="text-decoration:none;">
+                              <img src="img/svg/036-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="https://www.instagram.com/boy_riandy1104/" target="_blank" style="text-decoration:none;">
+                              <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                           
+                            
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ?>
